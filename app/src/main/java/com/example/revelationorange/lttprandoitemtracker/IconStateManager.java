8e72238a2f005b4ebf1e645e1746d417c9b597df
RevelationOrange <RevelationOrange@gmail.com>
2018-08-23 17:19:49 -0400

    added note buttons (markers for what dungeon gives what, and spells for entry) added iconAlpha array to track alpha values (since some buttons never fade) quite clunky though also another array to track note button states had to duplicate fxns for advancing and resetting states, for note buttons probly coulda passed a bool in, but... ehh implemented creation and placement for each icon button and onclick/onlongclick listeners, and reset button

--- app/src/main/java/com/example/revelationorange/lttprandoitemtracker/IconStateManager.java
+++ app/src/main/java/com/example/revelationorange/lttprandoitemtracker/IconStateManager.java
@@ -5,90 +5,173 @@ import android.content.SharedPreferences;
 import java.util.ArrayList;
 
 public class IconStateManager {
+    private ArrayList<Boolean[]> iconAlphas = new ArrayList<>();
     private ArrayList<Integer[]> iconBGs = new ArrayList<>();
     private ArrayList<Integer[]> noteIconBGs = new ArrayList<>();
     private ArrayList<Integer> iconStates = new ArrayList<>();
+    private ArrayList<Integer> noteIconStates = new ArrayList<>();
 
     IconStateManager(SharedPreferences sp) {
         this.iconBGs.add(new Integer[]{R.drawable.tunic1, R.drawable.tunic2, R.drawable.tunic3});
-        this.iconBGs.add(new Integer[]{R.drawable.sword1, R.drawable.sword2, R.drawable.sword3, R.drawable.sword4});
-        this.iconBGs.add(new Integer[]{R.drawable.bow0, R.drawable.bow1, R.drawable.bow2, R.drawable.bow3});
-        this.iconBGs.add(new Integer[]{R.drawable.boomerang1, R.drawable.boomerang2, R.drawable.boomerang3});
-        this.iconBGs.add(new Integer[]{R.drawable.hookshot});
-        this.iconBGs.add(new Integer[]{R.drawable.mushroom});
-        this.iconBGs.add(new Integer[]{R.drawable.powder});
+        this.iconAlphas.add(new Boolean[]{true, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.sword1, R.drawable.sword1, R.drawable.sword2, R.drawable.sword3, R.drawable.sword4});
+        this.iconAlphas.add(new Boolean[]{false, true, true, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.bow0, R.drawable.bow0, R.drawable.bow1, R.drawable.bow2, R.drawable.bow3});
+        this.iconAlphas.add(new Boolean[]{false, true, true, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.boomerang1, R.drawable.boomerang1, R.drawable.boomerang2, R.drawable.boomerang3});
+        this.iconAlphas.add(new Boolean[]{false, true, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.hookshot, R.drawable.hookshot});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.mushroom, R.drawable.mushroom});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.powder, R.drawable.powder});
+        this.iconAlphas.add(new Boolean[]{false, true});
         //
-        this.iconBGs.add(new Integer[]{R.drawable.shield1, R.drawable.shield2, R.drawable.shield3});
-        this.iconBGs.add(new Integer[]{R.drawable.moonpearl});
-        this.iconBGs.add(new Integer[]{R.drawable.firerod});
-        this.iconBGs.add(new Integer[]{R.drawable.icerod});
-        this.iconBGs.add(new Integer[]{R.drawable.bombos});
-        this.iconBGs.add(new Integer[]{R.drawable.ether});
-        this.iconBGs.add(new Integer[]{R.drawable.quake});
+        this.iconBGs.add(new Integer[]{R.drawable.shield1, R.drawable.shield1, R.drawable.shield2, R.drawable.shield3});
+        this.iconAlphas.add(new Boolean[]{false, true, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.moonpearl, R.drawable.moonpearl});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.firerod, R.drawable.firerod});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.icerod, R.drawable.icerod});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.bombos, R.drawable.bombos});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.ether, R.drawable.ether});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.quake, R.drawable.quake});
+        this.iconAlphas.add(new Boolean[]{false, true});
         //
-        this.iconBGs.add(new Integer[]{R.drawable.boss0});
+        this.iconBGs.add(new Integer[]{R.drawable.boss0, R.drawable.boss0});
+        this.iconAlphas.add(new Boolean[]{false, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest3, R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
-        this.iconBGs.add(new Integer[]{R.drawable.lantern});
-        this.iconBGs.add(new Integer[]{R.drawable.hammer});
-        this.iconBGs.add(new Integer[]{R.drawable.shovel});
-        this.iconBGs.add(new Integer[]{R.drawable.net});
-        this.iconBGs.add(new Integer[]{R.drawable.book});
+        this.iconAlphas.add(new Boolean[]{true, true, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.lantern, R.drawable.lantern});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.hammer, R.drawable.hammer});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.shovel, R.drawable.shovel});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.net, R.drawable.net});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.book, R.drawable.book});
+        this.iconAlphas.add(new Boolean[]{false, true});
         //
-        this.iconBGs.add(new Integer[]{R.drawable.boss1});
+        this.iconBGs.add(new Integer[]{R.drawable.boss1, R.drawable.boss1});
+        this.iconAlphas.add(new Boolean[]{false, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
+        this.iconAlphas.add(new Boolean[]{true, true, true});
         this.iconBGs.add(new Integer[]{R.drawable.bottle0, R.drawable.bottle1, R.drawable.bottle2, R.drawable.bottle3, R.drawable.bottle4});
-        this.iconBGs.add(new Integer[]{R.drawable.somaria});
-        this.iconBGs.add(new Integer[]{R.drawable.byrna});
-        this.iconBGs.add(new Integer[]{R.drawable.cape});
-        this.iconBGs.add(new Integer[]{R.drawable.mirror});
+        this.iconAlphas.add(new Boolean[]{false, true, true, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.somaria, R.drawable.somaria});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.byrna, R.drawable.byrna});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.cape, R.drawable.cape});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.mirror, R.drawable.mirror});
+        this.iconAlphas.add(new Boolean[]{false, true});
         //
-        this.iconBGs.add(new Integer[]{R.drawable.boss2});
+        this.iconBGs.add(new Integer[]{R.drawable.boss2, R.drawable.boss2});
+        this.iconAlphas.add(new Boolean[]{false, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
-        this.iconBGs.add(new Integer[]{R.drawable.boots});
-        this.iconBGs.add(new Integer[]{R.drawable.glove1, R.drawable.glove2});
-        this.iconBGs.add(new Integer[]{R.drawable.flippers});
-        this.iconBGs.add(new Integer[]{R.drawable.flute});
+        this.iconAlphas.add(new Boolean[]{true, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.boots, R.drawable.boots});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.glove1, R.drawable.glove1, R.drawable.glove2});
+        this.iconAlphas.add(new Boolean[]{false, true, true});
+        this.iconBGs.add(new Integer[]{R.drawable.flippers, R.drawable.flippers});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.flute, R.drawable.flute});
+        this.iconAlphas.add(new Boolean[]{false, true});
         this.iconBGs.add(new Integer[]{R.drawable.agahnim0, R.drawable.agahnim1});
+        this.iconAlphas.add(new Boolean[]{false, true});
         //
-        this.iconBGs.add(new Integer[]{R.drawable.boss3});
-        this.iconBGs.add(new Integer[]{R.drawable.boss4});
-        this.iconBGs.add(new Integer[]{R.drawable.boss5});
-        this.iconBGs.add(new Integer[]{R.drawable.boss6});
-        this.iconBGs.add(new Integer[]{R.drawable.boss7});
-        this.iconBGs.add(new Integer[]{R.drawable.boss8});
-        this.iconBGs.add(new Integer[]{R.drawable.boss9});
+        this.iconBGs.add(new Integer[]{R.drawable.boss3, R.drawable.boss3});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.boss4, R.drawable.boss4});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.boss5, R.drawable.boss5});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.boss6, R.drawable.boss6});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.boss7, R.drawable.boss7});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.boss8, R.drawable.boss8});
+        this.iconAlphas.add(new Boolean[]{false, true});
+        this.iconBGs.add(new Integer[]{R.drawable.boss9, R.drawable.boss9});
+        this.iconAlphas.add(new Boolean[]{false, true});
         //
         this.iconBGs.add(new Integer[]{R.drawable.chest5, R.drawable.chest4, R.drawable.chest3, R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
+        this.iconAlphas.add(new Boolean[]{true, true, true, true, true, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest6, R.drawable.chest5, R.drawable.chest4, R.drawable.chest3, R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
+        this.iconAlphas.add(new Boolean[]{true, true, true, true, true, true, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
+        this.iconAlphas.add(new Boolean[]{true, true, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest4, R.drawable.chest3, R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
+        this.iconAlphas.add(new Boolean[]{true, true, true, true, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest3, R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
+        this.iconAlphas.add(new Boolean[]{true, true, true, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
+        this.iconAlphas.add(new Boolean[]{true, true, true});
         this.iconBGs.add(new Integer[]{R.drawable.chest5, R.drawable.chest4, R.drawable.chest3, R.drawable.chest2, R.drawable.chest1, R.drawable.chest0});
+        this.iconAlphas.add(new Boolean[]{true, true, true, true, true, true});
 
-        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize1, R.drawable.prize2, R.drawable.prize3, R.drawable.prize4});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
+        this.noteIconBGs.add(new Integer[]{R.drawable.medallion0, R.drawable.bombos, R.drawable.ether, R.drawable.quake});
+        this.noteIconBGs.add(new Integer[]{R.drawable.prize0, R.drawable.prize3, R.drawable.prize4, R.drawable.prize2, R.drawable.prize1});
         this.noteIconBGs.add(new Integer[]{R.drawable.medallion0, R.drawable.bombos, R.drawable.ether, R.drawable.quake});
 
         for (int i = 0; i < iconBGs.size(); i++) {
             iconStates.add(sp.getInt(Integer.toString(i), 0));
         }
+        for (int i = 0; i < noteIconBGs.size(); i++) {
+            noteIconStates.add(sp.getInt(Integer.toString(iconBGs.size()+i), 0));
+        }
     }
 
     public int getNIcons() { return this.iconBGs.size(); }
+    public int getNNoteIcons() { return this.noteIconBGs.size(); }
 
     public int getState(int index) { return this.iconStates.get(index); }
+    public int getNoteState(int index) { return this.noteIconStates.get(index); }
 
     public int getBackground(int index) { return this.iconBGs.get(index)[this.iconStates.get(index)]; }
+    public int getNoteBackground(int index) { return this.noteIconBGs.get(index)[this.noteIconStates.get(index)]; }
 
-    public int advState(int index) {
+    public boolean getAlpha(int index) { return this.iconAlphas.get(index)[this.iconStates.get(index)]; }
+
+    public int[] advState(int index) {
         int nextState = this.iconStates.get(index)+1;
+        int retAlpha;
         if (nextState == this.iconBGs.get(index).length) { nextState = 0; }
         this.iconStates.set(index, nextState);
-        return nextState;
+        retAlpha = this.iconAlphas.get(index)[nextState] ? 1 : 0;
+        return new int[]{this.iconBGs.get(index)[this.iconStates.get(index)], retAlpha};
+    }
+    public int advNoteState(int index) {
+        int nextState = this.noteIconStates.get(index)+1;
+        if (nextState == this.noteIconBGs.get(index).length) { nextState = 0; }
+        this.noteIconStates.set(index, nextState);
+        return this.noteIconBGs.get(index)[this.noteIconStates.get(index)];
     }
 
-    public int resetState(int index) {
+    public int[] resetState(int index) {
+        int retAlpha;
         this.iconStates.set(index, 0);
-        return this.iconBGs.get(index)[0];
+        retAlpha = this.iconAlphas.get(index)[0] ? 1 : 0;
+        return new int[]{this.iconBGs.get(index)[0], retAlpha};
+    }
+    public int resetNoteState(int index) {
+        this.noteIconStates.set(index, 0);
+        return this.noteIconBGs.get(index)[0];
     }
 }
--- app/src/main/java/com/example/revelationorange/lttprandoitemtracker/MainActivity.java
+++ app/src/main/java/com/example/revelationorange/lttprandoitemtracker/MainActivity.java
@@ -1,21 +1,221 @@
 package com.example.revelationorange.lttprandoitemtracker;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sp;
+    private IconStateManager iconManager;
+    private ArrayList<ImageButton> allButtons = new ArrayList<>();
+    private ArrayList<ImageButton> allNoteButtons = new ArrayList<>();
+    private ArrayList<Integer> prizeNotesIDs = new ArrayList<>();
+    private int iconWidth, iconHeight;
+
+    private static final String SHARED_PREFS = "shared_preferences";
+    private final float MARKED_ALPHA = 1.0f, UNMARKED_ALPHA = 0.4f;
     private static int idStart = 100;
     private static int idCounter = idStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ConstraintLayout trackerPanel;
+        ConstraintSet cset = new ConstraintSet();
+        ImageButton prevButton = null;
+        int screenWidth, screenHeight, iconSizeRestriction, newRowCounter = 1, noteIndex = 0;
+        final int maxIcons = 7;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sp = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
 
+        // icon credit goes to crossproduct and halfarebel, I straight up just used the images
+        // from their item/map tracker
+
+        iconManager = new IconStateManager(sp);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels;
+        iconSizeRestriction = Math.min(screenWidth, screenHeight);
+        iconWidth = (int) Math.floor(iconSizeRestriction / maxIcons);
+        iconHeight = iconWidth;
+        Bitmap bmp = ((BitmapDrawable) getDrawable(R.drawable.bomb)).getBitmap();
+        Bitmap resized = Bitmap.createScaledBitmap(bmp, iconWidth, iconHeight, false);
+        findViewById(R.id.resetButton).setBackground(new BitmapDrawable(getResources(), resized));
+
+        prizeNotesIDs.add(14);
+        prizeNotesIDs.add(21);
+        prizeNotesIDs.add(28);
+        prizeNotesIDs.add(35);
+        prizeNotesIDs.add(36);
+        prizeNotesIDs.add(37);
+        prizeNotesIDs.add(38);
+        prizeNotesIDs.add(39);
+        prizeNotesIDs.add(40);
+        prizeNotesIDs.add(41);
+
+        trackerPanel = findViewById(R.id.trackerPanel);
+        findViewById(R.id.mainLayout).setBackgroundColor(Color.parseColor("#101010"));
+        for (int i = 0; i < iconManager.getNIcons(); i++) {
+            ImageButton b = makeImgButton(this);
+
+            bmp = ((BitmapDrawable) getDrawable(iconManager.getBackground(i))).getBitmap();
+            resized = Bitmap.createScaledBitmap(bmp, iconWidth, iconHeight, false);
+
+            b.setBackground(new BitmapDrawable(getResources(), resized));
+//            b.setBackgroundResource(iconManager.getBackground(i));
+            // setAlpha logic here
+            b.setAlpha(iconManager.getAlpha(i) ? MARKED_ALPHA : UNMARKED_ALPHA);
+
+            // set listeners here
+            b.setOnClickListener(cycleIcons(b, i, false));
+            b.setOnLongClickListener(resetIcon(b, i, false));
+
+            trackerPanel.addView(b);
+            cset.clone(trackerPanel);
+            if (prevButton == null) {
+                cset.connect(b.getId(), ConstraintSet.TOP, findViewById(R.id.resetButton).getId(), ConstraintSet.BOTTOM, 16*3);
+                cset.connect(b.getId(), ConstraintSet.LEFT, trackerPanel.getId(), ConstraintSet.LEFT);
+            }
+            else {
+                if (newRowCounter < maxIcons) {
+                    cset.connect(b.getId(), ConstraintSet.TOP, prevButton.getId(), ConstraintSet.TOP);
+                    cset.connect(b.getId(), ConstraintSet.LEFT, prevButton.getId(), ConstraintSet.RIGHT);
+                    newRowCounter++;
+                }
+                else {
+                    cset.connect(b.getId(), ConstraintSet.TOP, prevButton.getId(), ConstraintSet.BOTTOM);
+                    cset.connect(b.getId(), ConstraintSet.LEFT, trackerPanel.getId(), ConstraintSet.LEFT);
+                    newRowCounter = 1;
+                }
+            }
+            cset.applyTo(trackerPanel);
+            allButtons.add(b);
+
+            if (prizeNotesIDs.contains(i)) {
+                ImageButton noteButton = makeImgButton(this);
+                bmp = ((BitmapDrawable) getDrawable(iconManager.getNoteBackground(noteIndex))).getBitmap();
+                resized = Bitmap.createScaledBitmap(bmp, iconWidth/2, iconHeight/2, false);
+                noteButton.setBackground(new BitmapDrawable(getResources(), resized));
+                noteButton.setOnClickListener(cycleIcons(noteButton, noteIndex, true));
+                noteButton.setOnLongClickListener(resetIcon(noteButton, noteIndex, true));
+                trackerPanel.addView(noteButton);
+                noteIndex++;
+
+                cset.clone(trackerPanel);
+                cset.connect(noteButton.getId(), ConstraintSet.BOTTOM, b.getId(), ConstraintSet.BOTTOM);
+                cset.connect(noteButton.getId(), ConstraintSet.RIGHT, b.getId(), ConstraintSet.RIGHT);
+                cset.applyTo(trackerPanel);
+                allNoteButtons.add(noteButton);
+
+                if (i == 40 || i == 41) {
+                    ImageButton spellNoteButton = makeImgButton(this);
+                    bmp = ((BitmapDrawable) getDrawable(iconManager.getNoteBackground(noteIndex))).getBitmap();
+                    resized = Bitmap.createScaledBitmap(bmp, iconWidth/2, iconHeight/2, false);
+                    spellNoteButton.setBackground(new BitmapDrawable(getResources(), resized));
+                    spellNoteButton.setOnClickListener(cycleIcons(spellNoteButton, noteIndex, true));
+                    spellNoteButton.setOnLongClickListener(resetIcon(spellNoteButton, noteIndex, true));
+                    trackerPanel.addView(spellNoteButton);
+                    noteIndex++;
+
+                    cset.clone(trackerPanel);
+                    cset.connect(spellNoteButton.getId(), ConstraintSet.BOTTOM, b.getId(), ConstraintSet.BOTTOM);
+                    cset.connect(spellNoteButton.getId(), ConstraintSet.LEFT, b.getId(), ConstraintSet.LEFT);
+                    cset.applyTo(trackerPanel);
+                    allNoteButtons.add(spellNoteButton);
+                }
+            }
+
+            prevButton = b;
+        }
+    }
+
+    public void resetAll(View v) {
+        for (int i = 0; i < allButtons.size(); i++) {
+            int[] bga = iconManager.resetState(i);
+            int bg = bga[0], alpha = bga[1];
+            Bitmap bmp = ((BitmapDrawable) getDrawable(bg)).getBitmap();
+            Bitmap resized = Bitmap.createScaledBitmap(bmp, iconWidth, iconHeight, false);
+            allButtons.get(i).setBackground(new BitmapDrawable(getResources(), resized));
+            allButtons.get(i).setAlpha(alpha == 0 ? UNMARKED_ALPHA : MARKED_ALPHA);
+        }
+        for (int i = 0; i < allNoteButtons.size(); i++) {
+            int bg = iconManager.resetNoteState(i);
+            Bitmap bmp = ((BitmapDrawable) getDrawable(bg)).getBitmap();
+            Bitmap resized = Bitmap.createScaledBitmap(bmp, iconWidth/2, iconHeight/2, false);
+            allNoteButtons.get(i).setBackground(new BitmapDrawable(getResources(), resized));
+        }
+    }
+
+    View.OnClickListener cycleIcons(final ImageButton b, final int index, final boolean isNote) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int bg, alpha, iw = iconWidth, ih = iconHeight;
+                if (isNote) {
+                    bg = iconManager.advNoteState(index);
+                    alpha = 1;
+                    iw /= 2;
+                    ih /= 2;
+                }
+                else {
+                    int[] bgAlpha = iconManager.advState(index);
+                    bg = bgAlpha[0];
+                    alpha = bgAlpha[1];
+                }
+                Bitmap bmp = ((BitmapDrawable) getDrawable(bg)).getBitmap();
+                Bitmap resized = Bitmap.createScaledBitmap(bmp, iw, ih, false);
+                b.setBackground(new BitmapDrawable(getResources(), resized));
+                b.setAlpha(alpha == 0 ? UNMARKED_ALPHA : MARKED_ALPHA);
+            }
+        };
+    }
+
+    View.OnLongClickListener resetIcon(final ImageButton b, final int index, final boolean isNote) {
+        return new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int bg, alpha, iw = iconWidth, ih = iconHeight;
+                if (isNote) {
+                    bg = iconManager.resetNoteState(index);
+                    alpha = 1;
+                    iw /= 2;
+                    ih /= 2;
+                }
+                else {
+                    int[] bga = iconManager.resetState(index);
+                    bg = bga[0];
+                    alpha = bga[1];
+                }
+                Bitmap bmp = ((BitmapDrawable) getDrawable(bg)).getBitmap();
+                Bitmap resized = Bitmap.createScaledBitmap(bmp, iw, ih, false);
+                b.setBackground(new BitmapDrawable(getResources(), resized));
+                b.setAlpha(alpha == 0 ? UNMARKED_ALPHA : MARKED_ALPHA);
+                return true;
+            }
+        };
+    }
+
+    protected void onDestroy() {
+        for (int i = 0; i < iconManager.getNIcons(); i++) {
+            sp.edit().putInt(Integer.toString(i), iconManager.getState(i)).commit();
+        }
+        super.onDestroy();
     }
 
     private static int getNextID() { return idCounter++; }
